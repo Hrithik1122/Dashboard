456ee697917a3b62dd25626d9d1cf3a8903b2e33
Hrithik Bansal <hrithikbansal1122@gmail.com>
2023-03-24 05:25:42 +0530

    database connection

--- include/dbconfig.php
+++ include/dbconfig.php
@@ -9,7 +9,7 @@ if (session_status() == PHP_SESSION_NONE) {
 $server = "localhost";
 $username = "root";
 $password = "root";
-$database = "dashboard";
+$database = "database";
 
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 try {
